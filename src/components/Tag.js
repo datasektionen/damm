6c3687da9404939ec715f23a95d730aecc603c1f
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-10-21 12:09:42 +0200

    Change default tag font color

--- src/components/Tag.js
+++ src/components/Tag.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Tag = ({color = "#fff", backgroundColor = "#e83d84", text = "", hoverText="text"}) => {
     return (
-        <div className="tag" title={hoverText === "" ? "Ingen beskrivning" : hoverText} style={{backgroundColor: backgroundColor === "" ? "#e83d84" : backgroundColor, color: color === "" ? "#000" : color}}>
+        <div
+            className="tag"
+            title={hoverText === "" ? "Ingen beskrivning" : hoverText}
+            style={{
+                backgroundColor: backgroundColor === "" ? "#e83d84" : backgroundColor, color: color === "" ? "#fff" : color
+            }}
+        >
             {text}
         </div>
     )

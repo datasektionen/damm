51d8657d2f4f977c3a4462dc59e6b2fd190cfb72
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-11-11 19:38:16 +0100

    Beautify 404 and 401

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -8,7 +8,8 @@ const NotFound = () => (
         <div><h2>404 Not Found</h2></div>
         </div>
         <div style={{textAlign: "center", padding: "50px"}}>
-            Sidan hittades ej :(
+            <h3><b>Det här gick ju inte så bra...</b></h3>
+            <h4 style={{paddingTop: "20px"}}>Kanske stavade du fel i adressen, eller så har sidan flyttat.  Om du tror att detta är fel, kontakta <a href="#" target="_blank" rel="noopener noreferrer">sidaministratörer</a>, <a href="https://ior.slack.com" target="_blank" rel="noopener noreferrer">IOR</a> eller <a href="https://github.com/datasektionen/damm/issues/new" target="_blank" rel="noopener noreferrer">skapa en issue på github</a></h4>
             <p><Link to={ROUTES.HOME}>Hem</Link></p>
         </div>
     </div>

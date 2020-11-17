ae27e83b4aac27daee47d245332fc4b2615dba48
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-11-17 20:59:18 +0100

    Add defense against click-jacking

--- app.js
+++ app.js
@@ -40,6 +40,8 @@ app.use(function(req, res, next) {
    res.header("Access-Control-Allow-Origin", "*");
    res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+   // Defend against click-jacking
+   res.header("X-Frame-Options", "DENY")
    next();
 });
 

4e7dd70a9f047320677f4ab801202f46b30571fd
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-01-25 18:38:59 +0100

    Pls work

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/registerServiceWorker.js',
+  entry: './src/serviceworker2.js',
   output: {
     filename: 'service-worker.js',
     path: path.resolve(__dirname, 'public'),
-  },
+  }
 };

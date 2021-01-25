0931c09062f705dcc5922ef039939f990e0ed3cd
Axel Elmarsson <axel.elmarsson@gmail.com>
2021-01-25 18:47:19 +0100

    Add working service worker

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/serviceworker2.js',
+  entry: './src/serviceworker.js',
   output: {
     filename: 'service-worker.js',
     path: path.resolve(__dirname, 'public'),
